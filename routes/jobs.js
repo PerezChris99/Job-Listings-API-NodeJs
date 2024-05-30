9f05bc3bb0db293e8572a3457ce69dbac97c533a
unknown <kweeziperez712@gmail.com>
2024-05-30 13:02:27 +0300

    fixed directory error

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Job = require('./models/Jobs');
+const Job = require('../models/Jobs');
 
 // Get all jobs
 router.get('/', async (req, res) => {
